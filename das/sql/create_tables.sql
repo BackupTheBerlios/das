bb9a879a3373cae7feeb0b3bb70b2711e260b037
se4722 <se4722>
2005-06-20 15:16:12 +0000

    *** empty log message ***

--- das/sql/create_tables.sql
+++ das/sql/create_tables.sql
@@ -6,14 +6,20 @@ drop table zut2rez;
 drop table rezept;
 drop table zutat;
 drop table kategorie;
-drop table benutzergruppe;
-drop table gruppe;
+--drop table benutzergruppe;
 drop table benutzer;
+drop table gruppe;
 drop sequence seq;
 
 
 create sequence seq
     start with 1000;
+
+-- kuerzel: gru
+create table gruppe (
+   id varchar(50) primary key,
+   name varchar(100) not null unique
+);
 
 -- kuerzel: bzr
 create table benutzer (
@@ -26,12 +32,6 @@ create table benutzer (
 );
 
 create index i_bzr_gru_id on benutzer(gru_id);
-
--- kuerzel: gru
-create table gruppe (
-   id varchar(50) primary key,
-   name varchar(100) not null unique
-);
 
 /*
 -- kuerzel: bg
@@ -70,7 +70,7 @@ create table rezept (
    id int primary key,
    name varchar(100) not null unique,
    anleitung text,
-   bzr_login varchar(50) references benutzer on delete set null
+   bzr_id int references benutzer on delete set null
 );
 
 -- kuerzel: zr
@@ -101,20 +101,20 @@ create index i_za_all_id on zut2all(all_id);
 
 -- kuerzel: ba
 create table bzr2all (
-   bzr_login varchar(50) not null references benutzer on delete cascade,
-   all_id int not null references allergie on delete cascade,
-   primary key(bzr_login, all_id)
+   bzr_id int not null references benutzer,
+   all_id int not null references allergie,
+   primary key(bzr_id, all_id)
 );
 
+create index i_ba_bzr_id on bzr2all(bzr_id);
 create index i_ba_all_id on bzr2all(all_id);
-
 
 -- kuerzel: bew
 create table bewertung (
    id int primary key,
-   bzr_login varchar(50) not null references benutzer on delete cascade,
+   bzr_id int not null references benutzer on delete cascade,
    rating int not null
 );
 
-create index i_bew_bzr_login on bewertung(bzr_login);
+create index i_bew_bzr_id on bewertung(bzr_id);
 create index i_bew_rating on bewertung(rating);
